760a12cd6cf6a5aba1e830857226b26fa0b56865
Jane Costa <yberdysh@gmail.com>
2019-04-01 16:19:03 -0400

    bubble sort optimized

--- bubbleSort.js
+++ bubbleSort.js
@@ -61,15 +61,20 @@ function isSorted(arr){
 }
 
 function bubbleSortConcise(arr){
+  let noSwaps;
   for (let i = arr.length; i > 0; i--){
+    noSwaps = true;
     for (let j = 0; j < i - 1; j++){
       if (arr[j] > arr[j + 1]){
         //swap
         let temp = arr[j]
         arr[j] = arr[j + 1]
         arr[j + 1] = temp
+        noSwaps = false
       }
     }
+    if (noSwaps) break;
+    //if no swaps occured, we know aarray is in order and can break out of loop
   }
   return arr
 }
