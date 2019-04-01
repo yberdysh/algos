264a4c6a31cd23869c957d2b5743f0a15e1171e3
Jane Costa <yberdysh@gmail.com>
2019-04-01 16:16:29 -0400

    bubble sort concise

--- bubbleSort.js
+++ bubbleSort.js
@@ -59,3 +59,17 @@ function isSorted(arr){
   }
   return true;
 }
+
+function bubbleSortConcise(arr){
+  for (let i = arr.length; i > 0; i--){
+    for (let j = 0; j < i - 1; j++){
+      if (arr[j] > arr[j + 1]){
+        //swap
+        let temp = arr[j]
+        arr[j] = arr[j + 1]
+        arr[j + 1] = temp
+      }
+    }
+  }
+  return arr
+}
