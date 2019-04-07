b81019705c240b612cca23acab374ad33829c80a
Jane Costa <yberdysh@gmail.com>
2019-04-07 15:41:20 -0400

    added longestSubSequenceInString

--- slidingWindow.js
+++ slidingWindow.js
@@ -54,4 +54,79 @@ function creativeApproach(arr, n){
     return maxSum
   }
 
-console.log(udemyApproach([1, 2, 5, 2, 8, 1, 5], 4))
+
+// this doesn't actually work with edge cases
+  // function longestSequenceOfUniqueChars(string){
+  //   if (string.length < 1) return 0;
+  //   let seen = new Set
+  //   let count = 1
+  //   let max = 0;
+  //   seen.add(string[0])
+  //   for (let i = 1; i < string.length; i++){
+  //     if (!seen.has(string[i])){
+  //       seen.add(string[i])
+  //       count++
+  //     } else {
+  //       seen = new Set
+  //       seen.add(string[i])
+  //       if (count > max){
+  //         max = count
+  //       }
+  //     }
+  //   }
+  //   return max
+  // }
+
+
+
+
+function longestSequenceOfUniqueChars(string){
+	let seen = {}
+	let max = 0;
+	let longestSubstring = ""
+	let pointer = 0;
+	let substring = ""
+	for (let i = 0; i < string.length; i++){
+		if (seen[string[i]] === undefined){
+			substring += string[i]
+			if (substring.length > max){
+				max = substring.length
+				longestSubstring = substring
+			}
+		} else {
+			pointer = seen[string[i]] + 1
+			substring = string.slice(pointer, i + 1)
+		    if (substring.length > max){
+				max = substring.length
+				longestSubstring = substring
+			}
+		}
+		seen[string[i]] = i
+	}
+	return longestSubstring
+}
+
+// still doesn't work for all specs
+function longestDustringWithoutDuplication(string){
+	let lastSeen = {}
+	let longest = [0, 1]
+	let startIndex = 0
+	for (let i = 0; i < string.length; i++){
+		if (lastSeen[i] !== undefined){
+			startIndex = Math.max(startIndex, lastSeen[string[i]] + 1)
+		}
+		if (longest[1] - longest[0] < i + 1 - startIndex){
+			longest = [startIndex, i + 1]
+		}
+		lastSeen[string[i]] = i
+	}
+	return string.slice(longest[0], longest[1])
+}
+
+
+
+
+// console.log(udemyApproach([1, 2, 5, 2, 8, 1, 5], 4))
+// console.log(longestSequenceOfUniqueChars("bonjourmonami"))
+// console.log(longestSequenceOfUniqueChars("clementisacap"))
+console.log(longestSequenceOfUniqueChars("abccdeaabbcddef"))
