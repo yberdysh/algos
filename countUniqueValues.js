7604f9cb51dcaad03170ce128e8c418e8e52f113
Jane Costa <yberdysh@gmail.com>
2019-04-06 22:40:16 -0400

    added a unique solution

--- countUniqueValues.js
+++ countUniqueValues.js
@@ -30,3 +30,17 @@ function countUniqueValues(arr){
   }
   return count
 }
+
+function uniqueSolution(arr){
+  if (!arr.length) return 0
+  let i = 0;
+  for (let j = 1; j < arr.length; j++){
+    if (arr[i] !== arr[j]){
+      i++
+      arr[i] = arr[j]
+    }
+  }
+  return i + 1
+}
+
+console.log(uniqueSolution([]))
