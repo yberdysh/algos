6749a0268ec8e7eaef2b6e0fa80416ad5e48f1aa
Jane Costa <yberdysh@gmail.com>
2019-04-06 22:03:33 -0400

    anagrams

---
+++ anagrams.js
@@ -0,0 +1,29 @@
+// given two strings, write a function to determine if the second string is an anagram of the first
+
+// set up two objects, each associated with a string
+// strore letters as keys and frequencies as values
+// iterate over one object comparing to second
+// return false if key doesn't exist in second, or if frequency isn't the same
+
+function anagrams(an1, an2){
+  let frequencies1 = makeFrequencyHash(an1)
+  let frequencies2 = makeFrequencyHash(an2)
+  for (let key in frequencies1){
+    // if key doesn't exist in frequencies2, return false
+    if (!frequencies1[key]){
+      anagram = false
+    }
+    if (frequencies1[key] !== frequencies2[key]){
+      anagram = false
+    }
+  }
+  return true;
+}
+
+function makeFrequencyHash(string){
+  let frequencies = {}
+  for (let char of string){
+    frequencies[char] = (frequencies[char] + 1) || 1
+  }
+  return frequencies
+}
