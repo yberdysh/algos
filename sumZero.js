8456aabeee4578278fed5f2bc54062f3f81b011d
Jane Costa <yberdysh@gmail.com>
2019-04-06 22:22:52 -0400

    added comments

--- sumZero.js
+++ sumZero.js
@@ -3,6 +3,7 @@ function sumZero(arr){
   let largePointer = arr.length - 1
   while (smallPointer !== largePointer){
     // can also say while left < right
+    // will also stop before they equal each other
     let sum = arr[smallPointer] + arr[largePointer]
     if (sum === 0){
       return [arr[smallPointer], arr[largePointer]]
