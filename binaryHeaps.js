c30db8c4d2112507cdea8a902aa968257636c899
Jane Costa <yberdysh@gmail.com>
2019-04-07 22:30:34 -0400

    added udemy solution

--- binaryHeaps.js
+++ binaryHeaps.js
@@ -61,6 +61,24 @@ class MaxBinaryHeap{
     }
     return this.values
   }
+
+  // udemy Solution:
+      insert(element){
+        this.values.push(element);
+        this.bubbleUp();
+    }
+    bubbleUp(){
+        let idx = this.values.length - 1;
+        const element = this.values[idx];
+        while(idx > 0){
+            let parentIdx = Math.floor((idx - 1)/2);
+            let parent = this.values[parentIdx];
+            if(element <= parent) break;
+            this.values[parentIdx] = element;
+            this.values[idx] = parent;
+            idx = parentIdx;
+        }
+    }
 }
 
 let heap = new MaxBinaryHeap
