1ea996b799a1152dc5f2dde37928ef44c6118e98
Jane Costa <yberdysh@gmail.com>
2019-04-15 23:53:45 -0400

    balanced brackets

---
+++ balancedBrackets.js
@@ -0,0 +1,28 @@
+let key = {
+  "{": "}",
+  "[": "]",
+  "(": ")",
+  "}": "{",
+  "]": "[",
+  ")": "("
+}
+
+function balancedBrackets(string) {
+  debugger;
+  let stack = []
+  for (let i = 0; i < string.length; i++){
+    if (!stack.length){
+      stack.push(string[i])
+    } else {
+      if (stack[stack.length - 1] === key[string[i]]){
+        stack.pop()
+      } else {
+        stack.push(string[i])
+      }
+    }
+    return stack.length === 0
+  }
+}
+
+let brackets = "(()())((()()()))"
+console.log(balancedBrackets(brackets))
